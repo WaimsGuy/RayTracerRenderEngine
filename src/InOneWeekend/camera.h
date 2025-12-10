9da2901c902e4b5daae5d2ec8029b9c1dfa12887
Jordan Rios <riosjordan25@gmail.com>
2025-12-10 12:23:03 -0600

    Refactored ray color function with scattered reflectance

--- src/InOneWeekend/camera.h
+++ src/InOneWeekend/camera.h
@@ -2,6 +2,7 @@
 #define CAMERA_H
 
 #include "hittable.h"
+#include "material.h"
 
 class camera {
     public:
@@ -100,9 +101,13 @@ class camera {
 
 
             if (world.hit(r, interval(0.001, infinity), rec)) {
-                vec3 direction = rec.normal + random_unit_vector();
-                // You can adjust how much light is reflected by adjusting the below scalar
-                return 0.5 * ray_color(ray(rec.p, direction), depth-1, world); 
+                ray scattered;
+                color attenuation;
+                //Scatter ray
+                if (rec.mat->scatter(r, rec, attenuation, scattered)) {
+                    return attenuation * ray_color(scattered, depth-1, world);
+                } 
+                return color(0,0,0);
             }
 
             vec3 unit_direction = unit_vector(r.direction());
