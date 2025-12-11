dd5eb01140d53ea2632854bf5f8d048ab3e8f967
Jordan Rios <riosjordan25@gmail.com>
2025-12-11 13:45:34 -0600

    Added capability for moving sphere and refactored hit function to support moving.

--- src/InOneWeekend/sphere.h
+++ src/InOneWeekend/sphere.h
@@ -6,40 +6,46 @@
 
 class sphere : public hittable {
   public:
-    sphere(const point3& center, double radius, shared_ptr<material> mat) 
-      : center(center), radius(std::fmax(0,radius)), mat(mat) {}
+    //Stationary Sphere
+    sphere(const point3& static_center, double radius, shared_ptr<material> mat) 
+      : center(static_center, vec3(0,0,0)), radius(std::fmax(0,radius)), mat(mat) {}
 
+    //Moving Sphere
+    sphere(const point3& center1, const point3& center2, double radius, shared_ptr<material> mat) 
+      : center(center1, center2 - center1), radius(std::fmax(0,radius)), mat(mat) {}
+    
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
-        vec3 oc = center - r.origin();
-        auto a = r.direction().length_squared();
-        auto h = dot(r.direction(), oc);
-        auto c = oc.length_squared() - radius*radius;
+      point3 current_center = center.at(r.time());
+      vec3 oc = current_center - r.origin();
+      auto a = r.direction().length_squared();
+      auto h = dot(r.direction(), oc);
+      auto c = oc.length_squared() - radius*radius;
 
-        auto discriminant = h*h - a*c;
-        if (discriminant < 0)
-            return false;
+      auto discriminant = h*h - a*c;
+      if (discriminant < 0)
+        return false;
 
-        auto sqrtd = std::sqrt(discriminant);
+      auto sqrtd = std::sqrt(discriminant);
 
-        // Find the nearest root that lies in the acceptable range.
-        auto root = (h - sqrtd) / a;
-        if (!ray_t.surrounds(root)) {
-            root = (h + sqrtd) / a;
-            if (!ray_t.surrounds(root))
-                return false;
-        }
+      // Find the nearest root that lies in the acceptable range.
+      auto root = (h - sqrtd) / a;
+      if (!ray_t.surrounds(root)) {
+          root = (h + sqrtd) / a;
+          if (!ray_t.surrounds(root))
+              return false;
+      }
 
-        rec.t = root;
-        rec.p = r.at(rec.t);
-        vec3 outward_normal = (rec.p - center) / radius;
-        rec.set_face_normal(r, outward_normal);
-        rec.mat = mat;
+      rec.t = root;
+      rec.p = r.at(rec.t);
+      vec3 outward_normal = (rec.p - current_center) / radius;
+      rec.set_face_normal(r, outward_normal);
+      rec.mat = mat;
 
-        return true;
+      return true;
     }
 
   private:
-    point3 center;
+    ray center;
     double radius;
     shared_ptr<material> mat;
 };
