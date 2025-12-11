01fd9259ecbd52896067b3f998fbfd3d7ada9af5
Jordan Rios <riosjordan25@gmail.com>
2025-12-10 20:29:41 -0600

    Fully implemented glass material

--- src/InOneWeekend/material.h
+++ src/InOneWeekend/material.h
@@ -73,7 +73,7 @@ class dielectric : public material {
         bool cannot_refract = ri * sin_theta > 1.0;
         vec3 direction;
 
-        if (cannot_refract) {
+        if (cannot_refract || reflectance(cos_theta, ri) > random_double()) {
             direction = reflect(unit_direction, rec.normal);
         } else {
             direction = refract(unit_direction, rec.normal, ri);
@@ -87,6 +87,14 @@ class dielectric : public material {
     // Refractive index in vacuum or air, or the ratio of the material's refractive index over
     // the refractive index of the enclosing media
     double refraction_index;
+
+    // Using schlick's approximation for reflectance
+    static double reflectance(double cosine, double refraction_index)
+    {
+        auto r0 = (1 - refraction_index) / (1 + refraction_index);
+        r0 = r0*r0;
+        return r0 + (1-r0)*std::pow((1 - cosine),5);
+    }
 };
 
 #endif
